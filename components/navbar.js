d6ed18798dc9ee26ec13c4b98a21793937c6cc96
Flynn Duniho <flynn@harveyosity.org>
2023-03-17 22:17:53 -0400

    mostly finish homepage

---
+++ components/navbar.js
@@ -0,0 +1,35 @@
+class Navbar extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+connectedCallback() {
+    this.outerHTML = `
+    <nav class="navbar">
+
+        <ul class="navbar-list">
+
+          <li class="navbar-item">
+            <button class="navbar-link  active" data-nav-link>About</button>
+          </li>
+
+          <li class="navbar-item">
+            <button class="navbar-link" data-nav-link>Resume</button>
+          </li>
+
+          <li class="navbar-item">
+            <button class="navbar-link" data-nav-link>Portfolio</button>
+          </li>
+
+          <li class="navbar-item">
+            <button class="navbar-link" data-nav-link>Contact</button>
+          </li>
+
+        </ul>
+
+    </nav>
+    `;
+  }
+}
+
+customElements.define('custom-navbar', Navbar);
---
+++ components/sidebar.js
@@ -0,0 +1,105 @@
+class Sidebar extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+connectedCallback() {
+    this.outerHTML = `
+    <aside class="sidebar">
+
+      <div class="sidebar-info">
+
+        <figure class="avatar-box">
+          <img src="./images/my-avatar.png" alt="Flynn Duniho" width="80">
+        </figure>
+
+        <div class="info-content">
+          <h1 class="name" title="Flynn Duniho">Flynn Duniho</h1>
+
+          <p class="title">Computer Science Major</p>
+        </div>
+
+      </div>
+
+      <div class="sidebar-info_more">
+
+        <div class="separator"></div>
+
+        <ul class="contacts-list">
+
+          <li class="contact-item">
+
+            <div class="icon-box">
+              <ion-icon name="mail-outline"></ion-icon>
+            </div>
+
+            <div class="contact-info">
+              <p class="contact-title">Email</p>
+
+              <a href="mailto:fhduniho@wpi.edy" class="contact-link">fhduniho@wpi.edy</a>
+            </div>
+
+          </li>
+
+          <li class="contact-item">
+
+            <div class="icon-box">
+              <ion-icon name="phone-portrait-outline"></ion-icon>
+            </div>
+
+            <div class="contact-info">
+              <p class="contact-title">Phone</p>
+
+              <a href="tel:+14252411216" class="contact-link">+1 (425) 241-1216</a>
+            </div>
+
+          </li>
+
+          <li class="contact-item">
+
+            <div class="icon-box">
+              <ion-icon name="location-outline"></ion-icon>
+            </div>
+
+            <div class="contact-info">
+              <p class="contact-title">Location</p>
+
+              <address>Carnation, Washington, USA</address>
+            </div>
+
+          </li>
+
+        </ul>
+
+        <div class="separator"></div>
+
+        <ul class="social-list">
+
+          <li class="social-item">
+            <a href="https://github.com/FlynnD273" class="social-link">
+              <ion-icon name="logo-github"></ion-icon>
+            </a>
+          </li>
+
+          <li class="social-item">
+            <a href="https://www.linkedin.com/in/flynnduniho/" class="social-link">
+              <ion-icon name="logo-linkedin"></ion-icon>
+            </a>
+          </li>
+
+          <li class="social-item">
+            <a href="https://www.instagram.com/flynnd03/" class="social-link">
+              <ion-icon name="logo-instagram"></ion-icon>
+            </a>
+          </li>
+
+        </ul>
+
+      </div>
+
+    </aside>
+    `;
+  }
+}
+
+customElements.define('custom-sidebar', Sidebar);
