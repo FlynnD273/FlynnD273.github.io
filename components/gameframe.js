cbf844cfd0bc03a1059b59eff976d2f5b214a260
Flynn Duniho <flynn@harveyosity.org>
2024-11-13 00:04:28 -0500

    Add fix for iOS iFrames

--- components/gameframe.js
+++ components/gameframe.js
@@ -7,18 +7,31 @@ class GameFrame extends HTMLElement {
 
   connectedCallback() {
     const showGame = () => {
+      this.iframe?.remove();
       this.iframe = document.createElement("iframe");
       this.iframe.allowFullscreen = true;
       this.iframe.src = this.getAttribute("src");
       this.iframe.style.display = "block";
       this.appendChild(this.iframe);
-      this.iframe.requestFullscreen();
+      // From https://stackoverflow.com/a/63738422
+      if (this.iframe.requestFullScreen) {
+        this.iframe.requestFullScreen();
+      } else if (this.iframe.webkitRequestFullScreen) {
+        this.iframe.webkitRequestFullScreen();
+      } else if (this.iframe.mozRequestFullScreen) {
+        this.iframe.mozRequestFullScreen();
+      } else if (this.iframe.msRequestFullscreen) {
+        this.iframe.msRequestFullscreen();
+      } else if (this.iframe.webkitEnterFullscreen) {
+        this.iframe.webkitEnterFullscreen(); //for iphone this code worked
+      }
       this.iframe.onfullscreenchange = _ => hideGame();
     };
 
     const hideGame = () => {
       if (this.iframe.isFullscreen) {
         this.iframe.remove();
+        this.iframe = null;
       }
       this.iframe.isFullscreen = true;
     }
